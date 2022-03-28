af51e1421ef2258ee451e4b82a8242bdfbc07531
dispersion <anyta1901@gmail.com>
2022-03-28 23:10:27 +0300

    Решение тестов + поправки

--- src/test/java/ru/skypro/calculator/CalculatorTest.java
+++ src/test/java/ru/skypro/calculator/CalculatorTest.java
@@ -3,32 +3,35 @@ package ru.skypro.calculator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static ru.skypro.calculator.CalculatorTestConstants.FIRST_ARGUMENT;
-import static ru.skypro.calculator.CalculatorTestConstants.THIRD_ARGUMENT;
+
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static ru.skypro.calculator.CalculatorTestConstants.*;
 
 public class CalculatorTest {
-    private static final int TWO_ARGUMENT = 5;
     private final CalculatorServiceImpl out = new CalculatorServiceImpl();
 
     @Test
     public void shouldReturnResultOfSum() {
-        Assertions.assertEquals(THIRD_ARGUMENT, out.plus(FIRST_ARGUMENT, TWO_ARGUMENT));
+        Assertions.assertEquals(FIRST_ARGUMENT, out.plus(THIRD_ARGUMENT, SECOND_ARGUMENT));
     }
     @Test
     public void shouldReturnResultOfMinus() {
-        Assertions.assertEquals(THIRD_ARGUMENT, out.minus(FIRST_ARGUMENT, TWO_ARGUMENT));
+        Assertions.assertEquals(SECOND_ARGUMENT, out.minus(THIRD_ARGUMENT, SECOND_ARGUMENT));
     }
     @Test
     public void shouldReturnResultOfMultiple() {
-        Assertions.assertEquals(THIRD_ARGUMENT, out.multiply(FIRST_ARGUMENT, TWO_ARGUMENT));
+        Assertions.assertEquals(FIRST_ARGUMENT, out.multiply(FIRST_ARGUMENT, SECOND_ARGUMENT));
     }
     @Test
     public void shouldReturnResultOfDivide() {
-        Assertions.assertEquals(THIRD_ARGUMENT, out.divide(FIRST_ARGUMENT, TWO_ARGUMENT));
+        Assertions.assertEquals(FIRST_ARGUMENT, out.divide(FIRST_ARGUMENT, SECOND_ARGUMENT));
     }
     @Test
     public void shouldReturnResultOfDivideWithZero() {
-        Assertions.assertEquals(THIRD_ARGUMENT, out.minus(FIRST_ARGUMENT, TWO_ARGUMENT));
+
+//        Assertions.assertEquals(throw new RuntimeException(), out.minus(FIRST_ARGUMENT, THIRD_ARGUMENT));
+        assertThrows(new RuntimeException("Делить на ноль нельзя"), () -> out.divide(FIRST_ARGUMENT,ZERO_ARGUMENT));
     }
 
 }
--- src/test/java/ru/skypro/calculator/CalculatorTestConstants.java
+++ src/test/java/ru/skypro/calculator/CalculatorTestConstants.java
@@ -2,7 +2,7 @@ package ru.skypro.calculator;
 
 public class CalculatorTestConstants {
     public static final int FIRST_ARGUMENT = 3;
-    public static final int SECOND_ARGUMENT = 2;
-    public static final int THIRD_ARGUMENT = 5;
+    public static final int SECOND_ARGUMENT = 1;
+    public static final int THIRD_ARGUMENT = 2;
     public static final int ZERO_ARGUMENT = 0;
 }
